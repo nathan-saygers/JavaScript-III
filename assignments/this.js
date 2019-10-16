62ad0e40538baccffe3c951d6f0e0e31df7493ee
nathan-saygers <nathan.saygers@gmail.com>
2019-10-16 13:09:29 -0700

    initial commit with this.js questions

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,52 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window or Global binding - If you don't give context to 'this' it will apply to the console.
+* 2. Implicit binding - You don't specifically call out how 'this' will bind, but it is implied through context (written within a function etc.)
+* 3. New binding - 'this' is bound to values based on the creation of new objects from a constructor
+* 4. Explicit binding - 'this' is bound to a value through the .call or .apply methods
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
-
-// code example for Window Binding
+console.log(this);
 
 // Principle 2
-
-// code example for Implicit Binding
+const object = {
+  property: 'doohicky',
+  objectFlips: function(flipType) {
+    console.log(`${this.property} does a ${flipType}`);
+    console.log(this);
+  }
+};
+object.objectFlips('backflip');
 
 // Principle 3
+function CoolStudent(teacher) {
+  this.coolGreeting = 'yo ';
+  this.teacher = teacher;
+  this.backTalk = function()  {
+    console.log(this.coolGreeting + this.teacher);
+    console.log(this);
+  };
+}
 
-// code example for New Binding
+const davey = new CoolStudent('Mr. Hawkins');
+const hawkins = new CoolStudent('Davey!');
+
+davey.backTalk();
 
 // Principle 4
+function student(){
+  console.log(this.attendance);
+}
+
+let firstStudent = {
+  name: 'Jonnie',
+  attendance: 'Present!'
+}
+
+student.call(firstStudent);
 
 // code example for Explicit Binding
